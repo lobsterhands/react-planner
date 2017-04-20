b00a6791465ea09a899e8a1141720957f0744045
Lyle <denmanlp@gmail.com>
2017-04-20 01:46:25 -0500

    Add array to state with length 42 for each box on a calendar month. Add logic in render() to calculate which days belong in current month.

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -7,7 +7,7 @@ class Calendar extends React.Component {
     super(props);
     this.state = {
       date: new Date(),
-      calendarDays: 42,
+      calendarDays: new Array(42).fill(0),
       monthNames: ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December']
     };
@@ -16,17 +16,6 @@ class Calendar extends React.Component {
   // @month is 1-based (January = 1)
   getDaysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
-  }
-
-  renderDays() {
-    const calendarDays = this.state.calendarDays;
-
-    var calendarDaysSquares = [];
-    for (var i = 0; i < calendarDays; i++) {
-      calendarDaysSquares.push(<div key={i} className='calendar-day'></div>);
-    }
-
-    return calendarDaysSquares;
   }
 
   goBackInTime() {
@@ -42,7 +31,6 @@ class Calendar extends React.Component {
 
   goForwardInTime() {
     const date = this.state.date;
-    console.log(date.parse());
     const nextMonthDate = date.setMonth(date.getMonth() + 1);
 
     this.setState(function() {
@@ -68,6 +56,38 @@ class Calendar extends React.Component {
     return lastDay;
   }
 
+  getLastMonthDays() {
+    // Returns the number of days for the previous month
+    const date = this.state.date;
+    var firstDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    return firstDay;
+  }
+
+  // renderDays(firstDay, daysInMonth, daysLastMonth) {
+  //   // @firstDay: 0-based integer (0 = Sunday)
+  //   // @lastDay: 0-based integer (0 = Sunday)
+  //   // @daysLastMonth: number days in previous month
+  //
+  //   const calendarDays = this.state.calendarDays;
+  //
+  //   var calendarDaysSquares = [];
+  //   for (var i = 0; i < calendarDays; i++) {
+  //
+  //     let day;
+  //     if (i >= firstDay && i < daysInMonth) {
+  //       day = i;
+  //     } else if (i <= firstDay - 1) {
+  //       day = i + 1;
+  //     }
+  //
+  //     calendarDaysSquares.push(<div key={i} className='calendar-day'>
+  //     {day}
+  //     </div>);
+  //   }
+  //
+  //   return calendarDaysSquares;
+  // }
+
   render() {
     const currentDate = this.state.date;
     const monthNames = this.state.monthNames.slice();
@@ -77,24 +97,44 @@ class Calendar extends React.Component {
     const oneBaseMonth = zeroBaseMonth + 1;
     const currentYear = currentDate.getFullYear();
     const daysInMonth = this.getDaysInMonth(oneBaseMonth, currentYear);
+    const firstDayOfMonth = this.getFirstDayOfMonth();
+    const lastDayOfMonth = this.getLastDayOfMonth();
+    const daysLastMonth = this.getLastMonthDays();
 
     return (
       <div className="Calendar">
         <h2>Calendar</h2>
-        <div className='calendar-left' onClick={() => this.goBackInTime()}>
-          <p>&#8678;</p>
-        </div>
+          <p className='calendar-left' onClick={() => this.goBackInTime()}>&#8678;</p>
         <div className='calendar-right' onClick={() => this.goForwardInTime()}>
           <p>&#8680;</p>
         </div>
 
         <h3>{currentMonth} {currentYear}</h3>
-        <p>First day: {this.getFirstDayOfMonth() + ''}</p>
-        <p>Last day: {this.getLastDayOfMonth() + ''}</p>
+        <p>First day: {firstDayOfMonth}</p>
+        <p>Last day: {lastDayOfMonth}</p>
+        <p>Last month's last day: {daysLastMonth}</p>
         <h4>Days in this month: {daysInMonth}</h4>
           {
-            this.renderDays().map(function (div) {
-              return div;
+            // this.renderDays(firstDayOfMonth, daysInMonth, daysLastMonth)
+            this.state.calendarDays.map(function (day, index) {
+              let calDay = 0;
+              let belongsToCurrent;
+              if (index < firstDayOfMonth) {
+                calDay = daysLastMonth - (firstDayOfMonth - (index + 1));
+                belongsToCurrent = false;
+              } else if (index >= firstDayOfMonth && index < daysInMonth + firstDayOfMonth) {
+                calDay = index - (firstDayOfMonth - 1);
+                belongsToCurrent = true;
+              } else {
+                calDay = (index - (firstDayOfMonth - 1)) % daysInMonth;
+                belongsToCurrent = false;
+              }
+
+              return (
+                <div key={index} className={"calendar-day " + (belongsToCurrent ? "current" : "not-current") }>
+                {calDay}
+                </div>
+              )
             })
           }
       </div>
