a3a8bfc9097cb8214b7fff539c300bfab0c289d6
Lyle Denman <denmanlp@gmail.com>
2017-04-24 10:45:51 -0500

    Start refactor; Moving State to Planner

--- app/components/Planner.js
+++ app/components/Planner.js
@@ -8,6 +8,7 @@ class Planner extends React.Component {
 
   constructor() {
     super();
+
     this.state = {
       todos: new Array({
         activity: 'buy milk',
@@ -19,11 +20,13 @@ class Planner extends React.Component {
       monthNames: ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December'],
       dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      calendarDays: new Array(42).fill(0),
     }
   }
 
   handleEnter(e) {
     if (e.key === 'Enter') {
+      e.preventDefault();
 
       const isActivityUnique = this.state.todos.every((todo) => {
         return todo.activity !== e.target.value;
@@ -53,16 +56,29 @@ class Planner extends React.Component {
     }
   }
 
+  updateCalendarDays(newDays) {
+    this.setState(function() {
+      return {
+        calendarDays: newDays
+      }
+    })
+  }
+
   render() {
+    console.log(this.state.calendarDays);
+
     return (
       <div className="Planner">
         <h1>Planner</h1>
         <Clock monthNames={this.state.monthNames}/>
         <Todos todos={this.state.todos} inputHandler={(e) => this.handleEnter(e)} />
         <Calendar
+          updateCalendarDays={(days) => this.updateCalendarDays(days)}
           calendarDays={this.state.calendarDays}
           monthNames={this.state.monthNames}
-          dayNames={this.state.dayNames}/>
+          dayNames={this.state.dayNames}
+          trueDate={this.state.trueDate}
+          viewDate={this.state.viewDate}/>
       </div>
     )
   }
