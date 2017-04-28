851b21fbc306bc2d9149a92e4fd189f429245b58
Lyle Denman <denmanlp@gmail.com>
2017-04-28 18:44:22 -0500

    Init commit - will hold todo data for date / time chunk

---
+++ app/components/ScheduleRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ScheduleRow extends React.Component {
+
+  render() {
+    return (
+      <td >I'm a data row</td>
+    )
+  }
+}
+
+module.exports = ScheduleRow;
