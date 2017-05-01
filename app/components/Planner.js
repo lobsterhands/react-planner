be24f93197bbad7a7b4967095c60ad7bb6f0ba6c
Lyle Denman <denmanlp@gmail.com>
2017-05-01 00:47:54 -0500

    Fix typo in timeSlices

--- app/components/Planner.js
+++ app/components/Planner.js
@@ -45,13 +45,13 @@ class Planner extends React.Component {
       dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       timeIncrements: ['12am', '12:30am', '1am', '1:30am','2am', '2:30am','3am', '3:30am','4am', '4:30am',
                         '5am', '5:30am','6am', '6:30am','7am', '7:30am','8am', '8:30am','9am', '9:30am',
-                        '10am', '10:30am','11am', '11:30am','12pm', '12:30pm', '1am', '1:30pm','2pm',
+                        '10am', '10:30am','11am', '11:30am','12pm', '12:30pm', '1pm', '1:30pm','2pm',
                         '2:30pm','3pm', '3:30pm','4pm', '4:30pm', '5pm', '5:30pm','6pm', '6:30pm','7pm',
                         '7:30pm','8pm', '8:30pm','9pm', '9:30pm','10pm', '10:30pm','11pm', '11:30pm'],
     }
 
-    this.updateSelectedDate = this.updateSelectedDate.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.updateSelectedDate = this.updateSelectedDate.bind(this);
     this.updateTrueDate = this.updateTrueDate.bind(this);
   }
 
@@ -107,7 +107,7 @@ class Planner extends React.Component {
   }
 
   render() {
-    const { calendarView, dayNames, monthNames, selectedDate, timeIncrements, todos, trueDate } = this.state;
+    const {calendarView, dayNames, monthNames, selectedDate, timeIncrements, todos, trueDate} = this.state;
     let calendar = null;
     if (calendarView === 'month') {
       calendar = <CalendarMonth
