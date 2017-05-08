a5196f613a5669647d6dd330efef98dffdc2c363
Lyle Denman <denmanlp@gmail.com>
2017-05-08 01:15:42 -0500

    Selecting a calendar date onClick is now properly sync'd

--- app/components/CalendarMini.js
+++ app/components/CalendarMini.js
@@ -2,24 +2,25 @@ const React = require('react');
 const PropTypes = require('prop-types');
 
 class CalendarMini extends React.Component {
-
   constructor(props) {
     super(props);
 
     const viewDate = new Date(); // current date with day set to first day of month
-    viewDate.setDate(1);
+    viewDate.setHours(0, 0, 0, 0);
+    viewDate.setDate(1); // Set to first day of the month
 
     this.state = {
       viewDate: viewDate,
       miniDays: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
     };
 
+    this.handleClickSelectDate = this.handleClickSelectDate.bind(this);
   }
 
   getCalendarDays() {
     let {selectedDate, trueDate, todos} = this.props;
+    const {viewDate} = this.state;
 
-    const viewDate = this.state.viewDate;
     const firstDayOfMonth = this.getFirstDayOfMonth();
     const daysInMonth = this.getNumDaysInMonth();
 
@@ -50,7 +51,7 @@ class CalendarMini extends React.Component {
       }
 
       return (
-        <div key={calendarDate} onClick={() => this.props.updateSelectedDate(calendarDate)}
+        <div key={calendarDate} onClick={() => this.handleClickSelectDate(calendarDate)}
           className={"calendar-day-mini " +
             (currentDay ? "current-day-mini " : "" ) +
             (currentMonth ? "current-month-mini " : "not-current-month-mini ") +
@@ -65,10 +66,21 @@ class CalendarMini extends React.Component {
     return daysThisMonth;
   }
 
+  handleClickSelectDate(calendarDate) {
+    const newDate = new Date(calendarDate.getTime());
+    newDate.setDate(1);
+
+    this.setState({
+      viewDate: newDate
+    })
+
+    this.props.updateSelectedDate(calendarDate);
+  }
+
   getFirstDayOfMonth() {
     // Returns integer representing the first day of the month
     // 1 = Monday, 7 = Sunday
-    const viewDate = this.state.viewDate;
+    const {viewDate} = this.state;
     const firstDay = new Date(viewDate.getFullYear(), viewDate.getMonth(), 1).getDay();
 
     if (firstDay === 0) { // Avoid having Sunday start the month in the zeroth calendar square
@@ -79,16 +91,16 @@ class CalendarMini extends React.Component {
 
   getNumDaysInMonth() {
     // @month: 0-based (January = 0)
-    const year = this.state.viewDate.getFullYear();
-    const month = this.state.viewDate.getMonth();
+    const {viewDate} = this.state;
+    const year =  viewDate.getFullYear();
+    const month = viewDate.getMonth();
     return new Date(year, month + 1, 0).getDate();
   }
 
   goBackInTime() {
     // Display previous month's calendar
-    const viewDate = this.state.viewDate;
+    const {viewDate} = this.state;
     const newView = new Date(viewDate.getFullYear(), viewDate.getMonth() - 1, viewDate.getDate());
-    const calendarDays = this.getCalendarDays(newView);
 
     this.setState(function () {
       return {
@@ -99,7 +111,7 @@ class CalendarMini extends React.Component {
 
   goForwardInTime() {
     // Display next month's calendar
-    const viewDate = this.state.viewDate;
+    const {viewDate} = this.state;
     const newView = new Date(viewDate.getFullYear(), viewDate.getMonth() + 1, viewDate.getDate());
 
     this.setState(function () {
@@ -186,6 +198,20 @@ class CalendarMini extends React.Component {
       </div>
     )
   }
+}
+
+CalendarMini.propTypes = {
+  dayNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  monthNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedDate: PropTypes.instanceOf(Date).isRequired,
+  trueDate: PropTypes.instanceOf(Date).isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      activity: PropTypes.string,
+      date: PropTypes.instanceOf(Date)
+    })
+  ).isRequired,
+  updateSelectedDate: PropTypes.func.isRequired
 }
 
 module.exports = CalendarMini;
