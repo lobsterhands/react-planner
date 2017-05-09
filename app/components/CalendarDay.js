6635e5098fa03978707cd9c2910e34b1db0c2cc2
Lyle Denman <denmanlp@gmail.com>
2017-05-09 12:55:59 -0500

    Initial commit. Add PropTypes. Navigation working.

--- app/components/CalendarDay.js
+++ app/components/CalendarDay.js
@@ -1,16 +1,98 @@
 const React = require('react');
+const PropTypes = require('prop-types');
+const ScheduleData = require('./ScheduleData');
+const TimeSliceHeader = require('./TimeSliceHeader');
 
 class CalendarDay extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      viewDate: this.props.viewDate
+    }
+
+    this.goBackInTime = this.goBackInTime.bind(this);
+    this.goForwardInTime = this.goForwardInTime.bind(this);
+    this.updateSelectedDate = this.props.updateSelectedDate.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      viewDate: nextProps.viewDate
+    })
+  }
+
+  goBackInTime() {
+    const {viewDate} = this.state;
+    const newViewDate = new Date(viewDate.getFullYear(), viewDate.getMonth(), viewDate.getDate() - 1);
+    this.props.updateViewDate(newViewDate);
+  }
+
+  goForwardInTime() {
+    const {viewDate} = this.state;
+    const newViewDate = new Date(viewDate.getFullYear(), viewDate.getMonth(), viewDate.getDate() + 1);
+    this.props.updateViewDate(newViewDate);
+  }
+
+  renderRows() {
+    const {timeIncrements, todos, trueDate, viewDate} = this.props;
+
+    let rowContainer = timeIncrements.map((timeSlice, index) => {
+      return (
+        <tr className={"calendar-schedule-row"} key={index}>
+          <TimeSliceHeader key={timeSlice} timeSlice={timeSlice} />
+          <ScheduleData date={viewDate} todos={todos} timeSlice={timeSlice} trueDate={trueDate} key={timeSlice + 1} onClick={this.updateSelectedDate}/>
+        </tr>
+      )
+    })
+
+    return rowContainer;
   }
 
   render() {
+    const {viewDate} = this.state;
+    const {dayNames, monthNamesAbbr} = this.props;
+    const dayName = dayNames[viewDate.getDay()].slice(0,3);
+    const monthName = monthNamesAbbr[viewDate.getMonth()];
+    const dayNum = viewDate.getDate();
+
     return (
-      <h1>CalendarDay</h1>
+      <div className="CalendarDay">
+        <div className="year-month">
+          <div className='calendar-left' onClick={this.goBackInTime}>
+            &#8678;
+          </div>
+            <h3>{dayName} {monthName} {dayNum}</h3>
+          <div className='calendar-right' onClick={this.goForwardInTime}>
+            &#8680;
+          </div>
+        </div>
+        <table className='table-calendar-day'>
+          <tbody>
+            <tr className='day-names'>
+            </tr>
+            {this.renderRows()}
+          </tbody>
+        </table>
+      </div>
     )
   }
+}
+
+CalendarDay.propTypes = {
+  dayNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  monthNamesAbbr: PropTypes.arrayOf(PropTypes.string).isRequired,
+  timeIncrements: PropTypes.arrayOf(PropTypes.string).isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      activity: PropTypes.string,
+      date: PropTypes.instanceOf(Date)
+    })
+  ).isRequired,
+  trueDate: PropTypes.instanceOf(Date).isRequired,
+  updateSelectedDate: PropTypes.func.isRequired,
+  updateViewDate: PropTypes.func.isRequired,
+  viewDate: PropTypes.instanceOf(Date)
 }
 
 module.exports = CalendarDay;
