e5f1bb0c19745610c606b05255e769f368b87242
Lyle Denman <denmanlp@gmail.com>
2017-05-10 13:23:27 -0500

    Simplify Planner state. Import MockTodos

--- app/components/Planner.js
+++ app/components/Planner.js
@@ -8,22 +8,13 @@ const CalendarYear = require('./CalendarYear');
 const CalendarViewButton = require('./CalendarViewButton');
 const Clock = require('./Clock');
 const Todos = require('./Todos');
+const MockTodos = require('./mock_data/MockTodos');
 
 class Planner extends React.Component {
   constructor() {
     super();
-    const trueDate = new Date(); // current date with hr, min, sec, ms zeroed out
+    const trueDate = new Date(); // Zero out time for exact comparison via .getTime()
     trueDate.setHours(0, 0, 0, 0);
-
-    const viewDate = new Date();
-    viewDate.setHours(0, 0, 0, 0);
-
-    const testDate = new Date();
-    testDate.setHours(0,0,0,0);
-
-    const testDate2 = new Date();
-    testDate2.setHours(0,0,0,0);
-    testDate2.setDate(testDate.getDate() + 2);
 
     this.state = {
       calendarView: 'week',
@@ -32,25 +23,14 @@ class Planner extends React.Component {
           'July', 'August', 'September', 'October', 'November', 'December'],
       monthNamesAbbr: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       selectedDate: trueDate,
-      todos: new Array(
-        {
-          activity: 'read',
-          date: testDate,
-          time: '8am'
-        },
-        {
-          activity: 'return library book to the library so that i can return the book to the library',
-          date: testDate2,
-          time: '12pm'
-        }
-      ),
+      todos: MockTodos,
       timeIncrements: ['12am', '12:30am', '1am', '1:30am','2am', '2:30am','3am', '3:30am','4am', '4:30am',
         '5am', '5:30am','6am', '6:30am','7am', '7:30am','8am', '8:30am','9am', '9:30am',
         '10am', '10:30am','11am', '11:30am','12pm', '12:30pm', '1pm', '1:30pm','2pm',
         '2:30pm','3pm', '3:30pm','4pm', '4:30pm', '5pm', '5:30pm','6pm', '6:30pm','7pm',
         '7:30pm','8pm', '8:30pm','9pm', '9:30pm','10pm', '10:30pm','11pm', '11:30pm'],
-        trueDate: trueDate,
-      viewDate: viewDate
+      trueDate: trueDate,
+      viewDate: trueDate
     }
 
     this.updateSelectedDate = this.updateSelectedDate.bind(this);
@@ -66,7 +46,7 @@ class Planner extends React.Component {
   }
 
   updateTrueDate() {
-    const trueDate = new Date(); // current date with hr, min, sec, ms zeroed out
+    const trueDate = new Date();
     trueDate.setHours(0, 0, 0, 0);
     this.setState({
       trueDate: trueDate
