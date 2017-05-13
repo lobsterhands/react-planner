84c825b600d54e279f9eb017ec3197f6db37d371
Lyle Denman <denmanlp@gmail.com>
2017-05-12 20:17:35 -0500

    Minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-require('./index.css');
 const App = require('./components/App');
+require('./index.css');
 
 ReactDOM.render(
-  <App />, document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 );
