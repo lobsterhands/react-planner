dca7944194c5cab8017479c9a7aacd6ddb67e642
Lyle Denman <denmanlp@gmail.com>
2017-05-20 13:33:52 -0500

    Change ModalDate and ModalTime to stateless functional components

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,8 +1,26 @@
 import React from 'react';
 const PropTypes = require('prop-types');
+const ModalActivity = require('./ModalActivity');
+
+function ModalDate (props) {
+  return (
+    <p><b>Date:</b><br/> {props.date.toString().slice(0, 16)}</p>
+  )
+}
+ModalDate.propTypes = {
+	date: PropTypes.instanceOf(Date).isRequired
+}
+
+function ModalTime (props) {
+  return (
+      <p><b>Time:</b><br/> {props.time}</p>
+  )
+}
+ModalTime.propTypes = {
+	time: PropTypes.string.isRequired
+}
 
 class ModalContent extends React.Component {
-
   handleClick(event) {
     event.stopPropagation();
   }
@@ -17,16 +35,15 @@ class ModalContent extends React.Component {
 
     return (
         <div className="modal-content" onClick={(e) => this.handleClick(e)}>
-          {activity}
-          <p>Date: {todo.date.toString()}</p>
-          <p>Time: {todo.time}</p>
+          <ModalActivity activity={todo.activity} />
+          <ModalDate date={todo.date} />
+          <ModalTime time={todo.time} />
         </div>
     )
   }
 }
-
 ModalContent.propTypes = {
-
+  // TODO: update along with consistent todo formatting throughout app
 }
 
 module.exports = ModalContent;
